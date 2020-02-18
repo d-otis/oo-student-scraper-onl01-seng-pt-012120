80eea9b54652ae00d73eb1631b55eb0a532ccec8
Dan Foley <dan@dan-foley.com>
2020-02-18 18:10:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,9 +20,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |k,v|
-      self.send("#{k}=", v)
-    end
+    attributes_hash.each {|k,v| self.send("#{k}=", v) }
   end
 
   def self.all
